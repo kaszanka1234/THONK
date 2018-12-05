890b85aed533941ea2d5119f1149ac58bcccdb4b
kaszanka1234 <adriankodzis@op.pl>
2018-12-05 20:43:01 +0100

    keks

--- Core/Moderation/joke.cs
+++ Core/Moderation/joke.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Collections;
-using System.Reflection;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -9,8 +8,13 @@ using Discord.WebSocket;
 namespace THONK.Core.Moderation{
     public class Joke : ModuleBase<SocketCommandContext>{
         [Command("unban")]
-        public async Task unban([Remainder]string asd){
-            await Context.Channel.SendMessageAsync(":x: insufficient permission");
+        public async Task unban(IGuildUser user){
+            if(Context.User.Id != 245088596451786754){
+                await Context.Channel.SendMessageAsync(":x: insufficient permission");
+                return;
+            }
+            await Context.Channel.SendMessageAsync($"{user.Username} has been unbanned by {Context.User.Mention}");
+            await user.AddRoleAsync(Context.Guild.Roles.Where(x => x.Name == "Warlord").FirstOrDefault());
         }
     }
 }
