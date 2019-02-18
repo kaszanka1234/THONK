f5d03d9e51f97eb7a5c9ae5a6179a893f37a0ac9
kaszanka1234 <adriankodzis@op.pl>
2019-02-18 22:49:08 +0100

    removed unused test command

--- Core/DEV/testCommand.cs
+++ Core/DEV/testCommand.cs
@@ -16,26 +16,5 @@ namespace THONK.DEV{
         public async Task module(){
             await Context.Channel.SendMessageAsync("This is a test command");
         }
-        [Command("dump")]
-        public async Task dump(int count = 50){
-            if(!(Context.User as SocketGuildUser).GuildPermissions.ManageMessages){
-                await Context.Channel.SendMessageAsync(":x: Insufficent permissions");
-                return;
-            }
-            List<string> users = new List<string>();
-            var msgs = await Context.Channel.GetMessagesAsync(count).OfType<IReadOnlyCollection<IMessage>>().ToList();
-            foreach(var collection in msgs){
-                foreach(var msg in collection){
-                    string username = msg.Author.Username.ToString();
-                    if(!users.Contains(username)){
-                        users.Add(username);
-                    }
-                }
-            }
-            string userList = "";
-            users.ForEach(x => userList += $"{x}\n");
-            await Context.Channel.SendMessageAsync("Users dumped");
-            await Context.User.SendMessageAsync(userList);
-        }
     }
 }
